4188f2a9fe4de648f8713e573893ee8656dfd426
LeandroCantoDev <leandrocanto007@gmail.com>
2026-09-08 02:52:40 -0300

    file reconstruction with product registration loop

--- Modelagem_classes.py
+++ Modelagem_classes.py
@@ -3,35 +3,13 @@ client = []
 order = {
     'client': {},
     'items': [],
-    'boleto': {}
+    'boleto': {},
 }
 
 
-# -------- REGISTER CLIENT -------- #
-client_name = input('Digite o nome do cliente: ' )   #'Type the client name'
-def register_client(client, client_name):
-    client.append({'name': client_name})
-register_client(client, client_name)
-
-
-#------- SEARCH CLIENT --------
-def search_client(clients, client_name):
-    for client in clients:
-        if client['name'] == client_name:
-            return client
-client_found = search_client(client, client_name)
-# print(client_found)
-order['client'] = client_found
-
-
 # -------- REGISTER PRODUCT -------- #
-name = input("Digite o nome do produto: ")    #'Type the product name'
-price = float(input('Digite o valor do produto: '))    # 'Type the product value'
 def register_product(products, name, price):
     products.append({'name': name, 'price': price})
-
-register_product(products, name, price)
-# print(produtos)
 
 
 #--------- SEARCH PRODUCT ----------#
@@ -39,37 +17,24 @@ def search_product(products, name):
     for product in products:
         if product['name']  == name:
             return product
-product_found = search_product(products, name)
-# print(product_found)
+
+
+# -------- REGISTER CLIENT -------- #
+def register_client(client, client_name):
+    client.append({'name': client_name})
+
+
+#------- SEARCH CLIENT --------
+def search_client(clients, client_name):
+    for client in clients:
+        if client['name'] == client_name:
+            return client
+
 
 #--------- ADD ITEM----------#
-quantity = int(input('Digite a quantidade do item: '))         #'Type the item quantity'
 def add_item(order, product, quantity):
     order['items'].append({'product': product, 'quantity': quantity})
 
-add_item(order, product_found, quantity)
-
-while True:
-
-# ---------- ASK FOR MORE SOMETHING ----------#
-    more_something = input('Mais algum produto? ' ).upper()
-    if more_something.startswith('S'):
-        answer = True
-    elif more_something.startswith('N'):
-        answer = False
-        break
-    else:
-        print('Type Yes or No')
-
-    name = input('Escreva o produto: ')
-    price = float(input('Digite o valor do produto: '))    # 'Type the product value'
-    product_found = search_product(products, name)
-    if product_found is None:
-        print('Produto não existe')
-        continue
-        #--------- ADD ITEM----------#
-    quantity = int(input('Digite a quantidade do item: '))         #'Type the item quantity'
-    add_item(order, product_found, quantity)
 
 # ------- GENERATE SUMMARY -------- #
 def generate_summary(order):
@@ -79,18 +44,50 @@ def generate_summary(order):
         subtotal = item['product']['price'] * item['quantity']
         print(item['product']['name'], 'x', item['quantity'], '=', subtotal )
         total += subtotal
-    print('O total do pedido é: ', total)     # The total order is  
+    print('O total do pedido é: ', total)     # 'The total order is: '
 
+
+while True:
+    name = input("Digite o produto: ")
+    price = float(input("Digite o preço: "))
+    register_product(products, name, price)
+
+    other_product = input("Gostaria de adicionar mais um produto? ").upper()
+    if not other_product.startswith('S'):
+        break
+
+
+client_name = input('Digite o nome do cliente: ')
+register_client(client, client_name)
+client_found = search_client(client, client_name)
+order['client'] = client_found
+
+
+while True:
+    more_something = input('Gostaria de adicionar um produto? ').upper()
+    if more_something.startswith('N'):
+        break
+    else:
+        product = input('Digite o nome do produto: ')
+    product_found = search_product(products, product)
+    if product_found is None:
+        print("Produto não existe")
+        continue
+    quantity = int(input("Digite a quantidade: "))
+    add_item(order, product_found, quantity)
+
+print()
+print('Resumo da compra:')
 generate_summary(order)
 
-
-boleto = input('Fazer boleto? Sim ou Não: ').upper()
-if boleto.startswith('S'):
-    print('Fazer boleto')   # Make a ticket
+print()
+print('Fazer boleto?')
+print("Sim / Não")
+boleto = input("Fazer boleto? ").upper()
+if boleto.startswith("S"):
     answer = True
 else:
-    print('Não fazer boleto')       # Dont make a ticket
     answer = False
 order['boleto'] = answer
-print(answer)
+
 print(order)
